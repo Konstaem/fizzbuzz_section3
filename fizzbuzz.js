9b16639b83fe0228aa2b221accfc79e699224ef9
Morgan Benton <morgan.benton@gmail.com>
2016-03-23 09:08:46 -0400

    tests for fizz, buzz, fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,33 @@ exports.count = function(begin, end) {
 
 	var output = begin;
 	for (var i = begin; i < end; i += 1) {
-		output = output + " " + (i + 1);
+		output = output + " " + exports.fizzer((i + 1));
 	}
-
 
 	// return, e.g. "1 2 3 4 5"
 	return output;
+};
+
+exports.fizzer = function(num) {
+	// if(0 === num % 3) {
+	// 	return "fizz";
+	// }
+	// return num;
+	return 0 === num % 3 ? "fizz" : num;
+};
+
+exports.buzzer = function(num) {
+	// if(0 === num % 5) {
+	// 	return "buzz";
+	// }
+	// return num;
+	return 0 === num % 5 ? "buzz" : num;
+};
+
+exports.fizzbuzzer = function(num) {
+	// if(0 === num % 15) {
+	// 	return "fizzbuzz";
+	// }
+	// return num;
+	return 0 === num % 15 ? "fizzbuzz" : num;
 };
--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -2,8 +2,25 @@ var fb = require('../fizzbuzz');
 
 describe("A program that plays fizzbuzz", function() {
 
-	it("can count from 1 to 5", function() {
-		expect(fb.count(1,5)).toBe("1 2 3 4 5");
+	it("can count from 1 to 6", function() {
+		expect(fb.count(1,6)).toBe("1 2 fizz 4 buzz fizz");
 	});
 
+	it("can take a number and return 'fizz' for multiples of 3 and the number otherwise", function() {
+		expect(fb.fizzer(3)).toBe('fizz');
+		expect(fb.fizzer(5)).toBe(5);
+		expect(fb.fizzer(17)).toBe(17);
+	});
+
+	it("can take a number and return 'buzz' for multiples of 5 and the number otherwise", function() {
+		expect(fb.buzzer(5)).toBe('buzz');
+		expect(fb.buzzer(6)).toBe(6);
+		expect(fb.buzzer(17)).toBe(17);
+	});
+
+	it("can take a number and return 'fizzbuzz' for multiples of 15 and the number otherwise", function() {
+		expect(fb.fizzbuzzer(15)).toBe('fizzbuzz');
+		expect(fb.fizzbuzzer(6)).toBe(6);
+		expect(fb.fizzbuzzer(17)).toBe(17);
+	});
 });
