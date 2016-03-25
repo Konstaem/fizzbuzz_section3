54b9378903c4716fe11606d0e473b4e547163d65
Morgan Benton <morgan.benton@gmail.com>
2016-03-25 08:54:19 -0400

    complete

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -2,8 +2,9 @@ var fb = require('../fizzbuzz');
 
 describe("A program that plays fizzbuzz", function() {
 
-	it("can count from 1 to 6", function() {
+	it("can count from 1 to 6 and make appropriate substitutions", function() {
 		expect(fb.count(1,6)).toBe("1 2 fizz 4 buzz fizz");
+		expect(fb.count(10,15)).toBe("buzz 11 fizz 13 14 fizzbuzz");
 	});
 
 	it("can take a number and return 'fizz' for multiples of 3 and the number otherwise", function() {
